f955cc9491a8a205ff5ff991eb0d9bc8acfe505d
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-06-16 21:24:27 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,37 +1,51 @@
-<?php
-if (isset($_POST) && isset($_FILES) && isset($_FILES['testfile'])) {
-    $file_name = $_FILES['testfile']['name'];
-    $tmp_file = $_FILES['testfile']['tmp_name'];
-    $uploads_dir = 'uploads/';
-    $path_info = pathinfo($uploads_dir . $file_name);
-    if ($path_info['extension'] === 'json') {
-        move_uploaded_file($tmp_file, $uploads_dir . $file_name);
-        echo 'Спасибо, Ваш тест загружен!';
-        //header('Location: ' . 'list.php');
-    }else{
-        echo 'Извините, нужен файл с расширением JSON';
-    }
-}
-?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ru">
+
 <head>
+    <title>2.2 «Обработка форм» - Форма загрузки</title>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Загрузить тест</title>
+    <style>
+        .container { max-width: 950px; margin: 0 auto; }
+        h1 {margin-bottom: 0.2em;}
+        li {margin: 3px 0;}
+    </style>
 </head>
+
 <body>
+    <div class="container">
+        <h2>Меню:</h2>
+        <ul>
+            <li><a href="admin.php">Форма загрузки тестов</a></li>
+            <li><a href="list.php">Список тестов</a></li>
+        </ul>
 
-    <form method="post" enctype=multipart/form-data>
-        <input type=file name=testfile>
-        <input type=submit value=Загрузить>
-    </form>
-
-    <ul>
-        <li><a href="admin.php">Загрузить тест</a></li>
-        <li><a href="list.php">Список тестов</a></li>
-    </ul>
+        <h2>Форма:</h2>
+        <form method="post" enctype=multipart/form-data>
+            <input type=file name=test_file>
+            <input type=submit value=Загрузить>
+            <p>
+            <?php
+            if (isset($_FILES) && isset($_POST) && isset($_FILES['test_file'])) {
+                $file_name = $_FILES['test_file']['name'];
+                $tmp_file = $_FILES['test_file']['tmp_name'];
+                if (is_dir("server"))
+                {
+                    $server = 'uploads/';
+                    $path_info = pathinfo($server . $file_name);
+                    if ($path_info['extension'] === 'json') {
+                        move_uploaded_file($tmp_file, $server .$file_name);
+                        echo "<strong>Тест загружен на сервер</strong>";
+                        exit;
+                    }else {
+                        echo "<strong>Неверный формат (нужен .json)</strong>";
+                    };
+                } else {
+                    echo "Нет папки для загрузки тестов";
+                }
+            };
+            ?>
+            </p>
+        </form>
+    </div>
 </body>
 </html>
