ddbc3a11b43a28c6d01568b5d40400bf7007c22b
YSaranchuk <38591523+YSaranchuk@users.noreply.github.com>
2018-06-16 21:33:06 +0300

    Update test.php

--- test.php
+++ test.php
@@ -2,68 +2,95 @@
 $file_list = glob('uploads/*.json');
 $test = [];
 foreach ($file_list as $key => $file) {
-    if ($key == $_GET['test']) {
+    if ($key == $_GET['test']) {  
         $file_test = file_get_contents($file_list[$key]);
         $decode_file = json_decode($file_test, true);
         $test = $decode_file;
     }
 }
-$question = $test[0]['question'];
-$answers[] = $test[0]['answers'];
-// Считаем количество верных ответов
-$result_true = 0;
-foreach ($answers[0] as $item) {
-    if ($item['result'] === true) {
-        $result_true++;
-    }
-}
-$post_true = 0;
-$post_false = 0;
-if (count($_POST) > 0) {
-    // Проверяем и считаем правильность введенных ответов
-    foreach ($_POST as $key => $item) {
-        if ($answers[0][$key]['result'] === true) {
-            $post_true++;
-        }else{
-            $post_false++;
-        }
-    }
-    // Сравниваем и выводим результат
-    if ($post_true === $result_true && $post_false === 0) {
-        echo 'Правильно!';
-    }elseif ($post_true > 0 && $post_false > 0) {
-        echo 'Ну почти =)';
-    }else{
-        echo 'Вы ошиблись =(';
-    }
-}
+$questions = $test['questions']; 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ru">
 <head>
+    <title>2.2 «Обработка форм» - Тест </title>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Тест: <?=$question?></title>
+    <style>
+        .container { max-width: 950px; margin: 0 auto; }
+        h1 {margin-bottom: 0.2em;}
+        li {margin: 3px 0;}
+        .button {margin: 15px;}
+        .answer:hover {text-decoration: underline;}
+    </style>
 </head>
 <body>
+<div class="container">
+    <h2>Меню:</h2>
+    <ul>
+        <li><a href="admin.php">Форма загрузки тестов</a></li>
+        <li><a href="list.php">Список тестов</a></li>
+    </ul>
 
-<form method="post">
-    <fieldset>
-        <legend><?=$question?></legend>
-        <?php foreach ($answers[0] as $key => $item) : ?>
-            <label><input type="checkbox" name="<?=$key1."-".$key2;?>" value="<?=$key1."-".$key2;?>"> <?=$item['answer'];?></label>
-        <?php endforeach; ?>
-    </fieldset>
-    <input type="submit" value="Отправить">
-</form>
+    <h2>Тест: <?=$test['title']?></h2>
+    <form method="post">
+        <fieldset>
+            <?php
+            $post_true = 0; 
+            $post_false = 0; 
+            $result_true = 0; 
+            foreach ($questions as $key1 => $number) : 
+                $question = $number['question']; 
+                $answers[] = $number['answers']; 
+                ?>
+                <h4>Вопрос: <?=$question;?></h4>
+                <?php
+                foreach ($answers[$key1] as $key2 => $item) :
+                    if ($item['result'] === true) {
+                        $result_true++; 
+                    };
+                    if (count($_POST) > 0) { 
+                        $answers_key = "$key1-$key2"; 
+                        if (isset($_POST[$answers_key])) { 
+                            global $selected;
+                            $selected = "checked";
+                            if ($item['result'] === true) { 
+                                $post_true++; 
+                            } else {
+                                $post_false++; 
+                            }
+                        } else {
+                            $selected = "";
+                        }
+                    } else {
+                        $selected = "";
+                    };
+                    ?>
+                    <label class="answer">
+                        <input type="checkbox" name="<?php echo $key1."-".$key2;?>" value="<?php echo $key1."-".$key2;?>" <?=$selected?>>
+                        <?=$item['answer'];?>
+                    </label>
+                <?php
+                endforeach; 
+                ?>
 
-<ul>
-    <li><a href="admin.php">Загрузить тест</a></li>
-    <li><a href="list.php">Список тестов</a></li>
-</ul>
+            <?php
+            endforeach;
+            
+            if (count($_POST) > 0) {
+                if ($post_true === $result_true && $post_false === 0) {
+                    echo '<h4>Результат: Правильно!</h4>';
+                }elseif ($post_true > 0 && $post_false <> 0) {
+                    echo '<h4>Результат: Ну почти =) (попробуйте еще)</h4>';
+                }else{
+                    echo '<h4>Результат: Вы ошиблись =(</h4>';
+                }
+            }
+            ?>
+        </fieldset>
+        <input class="button" type="submit" value="Отправить">
+    </form>
 
+</div>
 </body>
 </html>
